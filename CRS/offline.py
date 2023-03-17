2c7e0be17a32cfb2ee87895a0c2a3af409a7dcec
Manya Madhu <97967173+Manya9388@users.noreply.github.com>
2023-03-17 10:41:10 +0530

    Add files via upload

--- CRS/offline.py
+++ CRS/offline.py
@@ -6,7 +6,7 @@ import numpy as np
 if __name__ == '__main__':
     fe = FeatureExtractor()
 
-    for img_path in sorted(Path("./static/img").glob("*.jfif")):
+    for img_path in sorted(Path("../xampp/htdocs/clothing/shopping/images1").glob("*.jfif")):
         print(img_path)  # e.g., ./static/img/xxx.jfif
         feature = fe.extract(img=Image.open(img_path))
         feature_path = Path("./static/feature") / (img_path.stem + ".npy")  # e.g., ./static/feature/xxx.npy
